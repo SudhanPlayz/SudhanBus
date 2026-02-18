df46f636afd2edd17e77daecb2b6ddb0a80829cb
NotWrench <notwrench401@gmail.com>
2026-02-18 18:53:54 +0530

    chore: `pnpm fix`

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -6,27 +6,32 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
+	database: drizzleAdapter(db, {
+		provider: "pg",
 
-    schema: schema,
-  }),
-  trustedOrigins: [
-    env.CORS_ORIGIN,
-    "mybettertapp://",
-    ...(env.NODE_ENV === "development"
-      ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
-      : []),
-  ],
-  emailAndPassword: {
-    enabled: true,
-  },
-  advanced: {
-    defaultCookieAttributes: {
-      sameSite: "none",
-      secure: true,
-      httpOnly: true,
-    },
-  },
-  plugins: [expo()],
+		schema,
+	}),
+	trustedOrigins: [
+		env.CORS_ORIGIN,
+		"mybettertapp://",
+		...(env.NODE_ENV === "development"
+			? [
+					"exp://",
+					"exp://**",
+					"exp://192.168.*.*:*/**",
+					"http://localhost:8081",
+				]
+			: []),
+	],
+	emailAndPassword: {
+		enabled: true,
+	},
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
+	plugins: [expo()],
 });
